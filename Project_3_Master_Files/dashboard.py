798d603bec28c32e37610e8a9c86faf062bd40ef
Richard Kell <rrkell1214@gmail.com>
2022-08-23 20:16:36 -0500

    Updated requirments

--- Project_3_Master_Files/dashboard.py
+++ Project_3_Master_Files/dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import sqlalchemy
+import yfinance as yf
 
 st.title('Financial Dashboard')
 
@@ -17,3 +18,12 @@ drop_down_II = st.selectbox('Choose a metric',
 values = df[df.sector == drop_down_I][drop_down_II]
 
 st.bar_chart(values)
+
+st.header('Company Information')
+input_ticker = st.text_input('Enter a stock symbol (all caps please)')
+st.write(input_ticker)
+x = yf.Ticker(input_ticker).history(period='max')
+st.write(x.describe())
+# st.write(x)
+close_df = x[['Close']].copy()
+st.line_chart(close_df)
