f8cb2840dc93cfde455697b01e0c5af52b6ec07b
cclogan619 <101228807+cclogan619@users.noreply.github.com>
2022-08-24 16:05:11 -0500

    Add files via upload
    Option Literature in VS Code

---
+++ Catherine_Files/Robinhood_Options_Training.py
@@ -0,0 +1,3 @@
+import streamlit as st 
+
+st.write("Learn More about Options [http://github.com/cclogan619/Project-3_Wisdom_Learners/Catherine_Files/Robinhood_Options_Training.pdf](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
