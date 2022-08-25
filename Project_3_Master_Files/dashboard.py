af56be698e0f824d3ee6520404af5d17f6c29c89
Richard Kell <rrkell1214@gmail.com>
2022-08-24 22:01:32 -0500

    Added Facebook Prophet Forcaster

--- Project_3_Master_Files/dashboard.py
+++ Project_3_Master_Files/dashboard.py
@@ -2,11 +2,17 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 
+from datetime import datetime
+from prophet import Prophet
+from prophet.plot import plot_plotly
+import numpy as np
+
 st.title('Financial Dashboard')
 
 
 
-df = pd.read_csv('Project_3_Master_Files/fundamentals.csv', index_col=['symbol'])
+# df = pd.read_csv('Project_3_Master_Files/fundamentals.csv', index_col=['symbol'])
+df = pd.read_csv('fundamentals.csv', index_col=['symbol'])
 
 drop_down_I = st.selectbox('Choose a sector',
                             df.sector.unique())
@@ -26,3 +32,28 @@ st.write(x.describe())
 # st.write(x)
 close_df = x[['Close']].copy()
 st.line_chart(close_df)
+
+
+forcast_df = close_df.reset_index()
+forcast_df.columns = ['ds', 'y']
+forcast_df = forcast_df.dropna()
+st.write(forcast_df.head())
+st.write(forcast_df.tail())
+
+m = Prophet()
+m.fit(forcast_df)
+future = m.make_future_dataframe(periods=8760, freq='H')
+forcast = m.predict(future)
+st.subheader('Forcast Data')
+st.write(forcast.tail())
+
+st.title('1 Year Forcast - Facebook Prophet')
+st.write('forcast data')
+fig1 = plot_plotly(m, forcast)
+st.plotly_chart(fig1)
+
+st.write('forcast components')
+fig2 = m.plot_components(forcast)
+st.write(fig2)
+
+st.write("Learn More about Options [http://github.com/cclogan619/Project-3_Wisdom_Learners/Catherine_Files/Robinhood_Options_Training.pdf](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
