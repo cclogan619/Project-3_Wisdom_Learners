da08162c493cf0c301941c52029b7dd4dc02d3b1
Richard Kell <rrkell1214@gmail.com>
2022-08-25 08:17:08 -0500

    Revised requirements

--- Project_3_Master_Files/dashboard.py
+++ Project_3_Master_Files/dashboard.py
@@ -50,7 +50,7 @@ st.write(forcast.tail())
 
 st.title('Forcast - Facebook Prophet')
 st.write('forcast data')
-fig1 = plot_plotly(m, forcast)
+fig1 = plot_plotly(forcast)
 st.plotly_chart(fig1)
 
 st.write('forcast components')
